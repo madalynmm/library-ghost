8fd070a51beaeea50c12e19951fd427e34a68808
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-01-05 21:58:38 -0500

    added home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,23 @@
 const router = require('express').Router();
-// const Wishlist = require('../../models/Books')
 
+// route to get search results
+router.get('/', async (req, res) => {
+    res.render('book-search');
+});
+
+// route to get one dish
+router.get('/collection', async (req, res) => {
+    try {
+        const dishData = await Dish.findByPk(req.params.id);
+        if (!dishData) {
+            res.status(404).json({ message: 'No dish with this id!' });
+            return;
+        }
+        const dish = dishData.get({ plain: true });
+        res.render('dish', dish);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
